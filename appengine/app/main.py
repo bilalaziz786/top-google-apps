16f557dba8cb53da73f5903d4678f94ddb0d8315
Mohd Bilal Aziz <mohdbilalaziz@Mohds-MacBook-Pro.local>
2020-10-22 20:12:58 +0530

    Web App2 framework

--- appengine/app/main.py
+++ appengine/app/main.py
@@ -12,18 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, render_template
+import os
+
+os.environ["DATASTORE_DATASET"] = "top-gle-apps"
+os.environ["DATASTORE_EMULATOR_HOST"] = "127.0.0.1:8081"
+os.environ["DATASTORE_EMULATOR_HOST_PATH"] = "127.0.0.1:8081/datastore"
+os.environ["DATASTORE_HOST"] = "http://127.0.0.1:8081"
+os.environ["DATASTORE_PROJECT_ID"] = "top-gle-apps"
+
+import six
+reload(six)
+import webapp2
 import play_scraper
 
 # [START gae_python38_datastore_store_and_fetch_times]
 from google.cloud import datastore
+from google.appengine.ext.webapp import template
 
-# from google.appengine.api import memcache
+from google.appengine.api import memcache
 
 
 datastore_client = datastore.Client()
 
-app = Flask(__name__)
+
+# app = Flask(__name__)
 
 
 def previous_apps_in_db():
@@ -93,39 +105,41 @@ def fetch_apps_from_cache():
 
 
 # [START gae_python38_datastore_render_times]
-@app.route('/')
-def root():
-    # top_apps = play_scraper.search('top', page=2)
-    # Store the current access time in Datastore.
-    # store_time(top_apps)
-    # Fetch the most recent 10 access times from Datastore.
-    # top_apps = fetch_apps_from_cache()
-    top_apps = None
-    if not top_apps:
-        top_apps = fetch_apps_from_datastore()
-        # store_in_cache(top_apps)
-    return render_template(
-        'index.html', top_apps=top_apps)
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # Fetch the most recent apps from Cache.
+        # top_apps = fetch_apps_from_cache()
+        top_apps = None
+        if not top_apps:
+            top_apps = fetch_apps_from_datastore()
+            # store_in_cache(top_apps)
+        # return render_template(
+        #     'index.html', top_apps=top_apps)
+        top_apps_temp = {'top_apps': top_apps}
+        self.response.out.write(template.render('templates/index.html', top_apps_temp))
 
 
-@app.route('/scrape', methods=['POST'])
-def re_scrape():
-    top_apps = play_scraper.search('top', page=2)
-    # Store the current access time in Datastore.
-    store_apps(top_apps)
-    # Fetch the most recent 10 access times from Datastore.
-    return render_template(
-        'index.html', top_apps=top_apps)
+class ReScrape(webapp2.RequestHandler):
+    def post(self):
+        top_apps = play_scraper.search('top', page=2)
+        # Store the current access time in Datastore.
+        store_apps(top_apps)
+        # Fetch the most recent 10 access times from Datastore.
+        # return render_template(
+        #     'index.html', top_apps=top_apps)
+        top_apps_temp = {'details': top_apps}
+        self.response.out.write(template.render('index.html', top_apps_temp))
 
 
-@app.route('/get_details/<app_id>')
-def get_app_details(app_id):
-    details = fetch_app_details(app_id)[0]
-    return render_template('details.html', details=details)
+# @app.route('/get_details/<app_id>')
+# def get_app_details(app_id):
+#     details = fetch_app_details(app_id)[0]
+#     return render_template('details.html', details=details)
 
 
 # [END gae_python38_datastore_render_times]
 
+app = webapp2.WSGIApplication([('/', MainPage), ('/scrape', ReScrape)], debug=True)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
@@ -136,4 +150,4 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
